282e8eaf6903e2b020b863f1e9bce919007593fe
julian <julian5595@outlook.com>
2024-03-07 20:37:59 +0000

    Rename variables to make more sense

--- hello_world/hello_world.py
+++ hello_world/hello_world.py
@@ -5,17 +5,15 @@ import depthai as dai
 pipeline = dai.Pipeline()
 
 # Define source - color camera
-cam_rgb = pipeline.createMonoCamera()
-#cam_rgb.setPreviewSize(1080, 720)
-#cam_rgb.setInterleaved(False)
-cam_rgb.setFps(40)
-cam_rgb.setBoardSocket(dai.CameraBoardSocket.CAM_B)
+mono = pipeline.createMonoCamera()
+mono.setFps(40)
+mono.setBoardSocket(dai.CameraBoardSocket.CAM_B)
 
 
 # Create output
 xout = pipeline.createXLinkOut()
-xout.setStreamName("left")
-cam_rgb.out.link(xout.input)
+xout.setStreamName("left_camera")
+mono.out.link(xout.input)
 
 # Pipeline defined, now the device connection
 with dai.Device(pipeline) as device:
@@ -23,13 +21,13 @@ with dai.Device(pipeline) as device:
     device.startPipeline()
 
     # Output queue will be used to get the frames from the output defined above
-    q_rgb = device.getOutputQueue(name="left", maxSize=4, blocking=False)
+    queue_mono = device.getOutputQueue(name="left_camera", maxSize=4, blocking=False)
 
     while True:
-        in_rgb = q_rgb.get()
-        if in_rgb is not None:
-            # Handle rgb frame (e.g., display it)
-            frame = in_rgb.getCvFrame()
+        in_mono = queue_mono.get()
+        if in_mono is not None:
+            # Handle mono frame (e.g., display it)
+            frame = in_mono.getCvFrame()
             # ... (image processing or display)
             cv2.imshow("preview", frame)
 
